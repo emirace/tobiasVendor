7636a3291fd6b23cd21b3e72860d604273d76fa9
Emirace <emmanuelakwuba57@gmail.com>
2022-04-18 15:44:11 +0100

    stage02

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 679px x 829
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numberOfReview: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas slim shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.5,
+      numberOfReview: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike slim pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numberOfReview: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas fit pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numberOfReview: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+
+export default data;
