66e57675194f1df054f5445c6435b162797130b0
emirace <emmanuelakwuba57@gmail.com>
2022-05-30 22:47:15 +0100

    hdn

--- frontend/src/component/Model.js
+++ frontend/src/component/Model.js
@@ -1,72 +1,67 @@
-import { faClose } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useRef } from 'react';
-import styled from 'styled-components';
-import ReviewLists from '../screens/ReviewLists';
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useRef } from "react";
+import styled from "styled-components";
+import ReviewLists from "../screens/ReviewLists";
 
 const Background = styled.div`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.8);
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 10;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10;
 `;
 
 const ModelWrapper = styled.div`
-  width: 50%;
-  height: 500px;
-  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-  background: #fff;
-  color: #000;
-  position: relative;
-  z-index: 10;
-  border-radius: 10px;
+    width: 50%;
+    height: 500px;
+    box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+    background: #fff;
+    color: #000;
+    position: relative;
+    z-index: 10;
+    border-radius: 10px;
 `;
 
 const CloseModelButton = styled.div`
-  cursor: pointer;
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  width: 32px;
-  height: 32px;
-  padding: 0;
-  z-index: 10;
-`;
-const G = styled.div`
-  background: blue;
+    cursor: pointer;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 32px;
+    height: 32px;
+    padding: 0;
+    z-index: 10;
 `;
 
 export default function Model({ showModel, setShowModel }) {
-  const modelRef = useRef();
+    const modelRef = useRef();
 
-  const closeModel = (e) => {
-    if (modelRef.current === e.target) {
-      setShowModel(false);
-    }
-  };
-  return (
-    <>
-      {showModel && (
-        <Background ref={modelRef} onClick={closeModel}>
-          <ModelWrapper showModel={showModel}>
-            jkfjgkdjd
-            <G></G>
-            <ReviewLists />
-            <CloseModelButton
-              ariel-label="Close model"
-              onClick={() => setShowModel(!showModel)}
-            >
-              <FontAwesomeIcon icon={faClose} />
-            </CloseModelButton>
-          </ModelWrapper>
-        </Background>
-      )}
-    </>
-  );
+    const closeModel = (e) => {
+        if (modelRef.current === e.target) {
+            setShowModel(false);
+        }
+    };
+    return (
+        <>
+            {showModel && (
+                <Background ref={modelRef} onClick={closeModel}>
+                    <ModelWrapper showModel={showModel}>
+                        <ReviewLists />
+                        <CloseModelButton
+                            ariel-label="Close model"
+                            onClick={() => setShowModel(!showModel)}
+                        >
+                            <FontAwesomeIcon icon={faClose} />
+                        </CloseModelButton>
+                    </ModelWrapper>
+                </Background>
+            )}
+        </>
+    );
 }
--- frontend/src/screens/ReviewLists.js
+++ frontend/src/screens/ReviewLists.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
+import React, { useContext, useState } from "react";
+import { Store } from "../Store";
 
 const Container = styled.div`
-  background: red;
+  
   height: 100%;
   width: 100%;
+  padding;10px;
+`;
+
+const Tab = styled.div`
+    display: flex;
+    margin-bottom: 5ps;
+    justify-content: center;
+`;
+const Content = styled.div``;
+const TabItem = styled.div`
+    display: flex;
+    justify-content: crnter;
+    cursor: pointer;
+    margin: 10px;
+    position: relative;
+    min-width: 50px;
+    &:hover {
+        color: var(--orange-color);
+    }
+    &.active {
+        color: var(--orange-color);
+        font-weight: bold;
+        &::after {
+            content: "";
+            position: absolute;
+            bottom: -10px;
+            left: 0;
+            width: 100%;
+            height: 2px;
+            background: var(--orange-color);
+        }
+    }
 `;
 
 export default function ReviewLists() {
-  return <Container></Container>;
+    const { state } = useContext(Store);
+    const { mode } = state;
+
+    const [displayTab, setDisplayTab] = useState("sold");
+
+    const tabSwitch = (tab) => {
+        switch (tab) {
+            case "sold":
+                return <>sold reviews</>;
+            case "purchase":
+                return <>purchase reviwws</>;
+            default:
+                return <></>;
+        }
+    };
+
+    return (
+        <Container>
+            <Tab>
+                <TabItem
+                    className={displayTab === "sold" && "active"}
+                    onClick={() => setDisplayTab("sold")}
+                >
+                    Sold
+                </TabItem>
+                <TabItem
+                    className={displayTab === "purchase" && "active"}
+                    onClick={() => setDisplayTab("purchase")}
+                >
+                    Purchase
+                </TabItem>
+            </Tab>
+            <Content>{tabSwitch(displayTab)}</Content>
+        </Container>
+    );
 }
