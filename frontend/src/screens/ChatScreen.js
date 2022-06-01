a17558b31b36a0f9b853492784b8f8724dbfe652
Emirace <emmanuelakwuba57@gmail.com>
2022-06-01 07:59:31 +0100

    h

--- frontend/src/screens/ChatScreen.js
+++ frontend/src/screens/ChatScreen.js
@@ -18,6 +18,7 @@ const ChatCont = styled.div`
   width: 100%;
   height: 100%;
   border-top: 1px solid rgba(99, 91, 91, 0.2);
+  padding: 5vw 7vw;
 `;
 const Left = styled.div`
   flex: 1;
@@ -25,6 +26,7 @@ const Left = styled.div`
 `;
 const Right = styled.div`
   flex: 2;
+  padding: 10px 30px;
 `;
 
 const TopBar = styled.div`
@@ -32,12 +34,108 @@ const TopBar = styled.div`
   padding: 15px 25px;
   justify-content: space-between;
   & svg {
+    position: relative;
     padding-right: 10px;
   }
 `;
 
+const Search = styled.input.attrs((props) => ({
+  placeholder: props.placeholder,
+}))`
+  background: black;
+  color: white;
+  border: 0;
+  border-radius: 25rem;
+  &::placeholder {
+    padding-left: 10px;
+    color: white;
+  }
+`;
+const ProfileImg = styled.img.attrs((props) => ({
+  src: props.src,
+}))`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: top;
+  margin-right: 15px;
+`;
+const ProfileDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const Name = styled.div`
+  color: var(--orange-color);
+  margin-bottom: 5px;
+  font-weight: bold;
+  text-transform: capitalize;
+`;
+const LastMsg = styled.div`
+  font-size: 14px;
+  width: 250px;
+  overflow: hidden;
+  display: inline-block;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
 const User = styled.div`
   padding: 15px 25px;
+  display: flex;
+  align-items: center;
+  &.active {
+    border-bottom: 2px solid var(--orange-color);
+  }
+`;
+
+const RightTopbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const LeftBar = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const SmallImg = styled.img.attrs((props) => ({
+  src: props.src,
+}))`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: top;
+  margin-right: 15px;
+`;
+const SmallName = styled.div`
+  text-transform: capitalize;
+`;
+const RightBar = styled.div`
+  text-transform: capitalize;
+`;
+const ChatArea = styled.div`
+  padding: 30px 0;
+`;
+const RecievedChat = styled.div`
+  display: flex;
+  margin-right: 500px;
+  justify-content: start;
+  padding: 20px;
+  background: var(--malon-color);
+  color: #fff;
+  margin-bottom: 15px;
+  border-radius: 10px;
+`;
+const SendChat = styled.div`
+  display: flex;
+  justify-content: end;
+  padding: 20px;
+  background: var(--orange-color);
+  color: #fff;
+  border-radius: 10px;
+  margin-bottom: 15px;
 `;
 
 export default function ChatScreen() {
@@ -70,18 +168,56 @@ export default function ChatScreen() {
           <TopBar>
             <div>
               <FontAwesomeIcon icon={faSearch} />
-              Search...
+              <Search placeholder="Search..." />
             </div>
             <div>
               <FontAwesomeIcon icon={faMessage} />
             </div>
           </TopBar>
-          <User>User Name</User>
-          <User>User Name</User>
-          <User>User Name</User>
-          <User>User Name</User>
+          <User className="active">
+            <ProfileImg src={'/images/pro.jpg'} />
+            <ProfileDetail>
+              <Name>John Doe</Name>
+              <LastMsg>
+                it is nice talking eith ice talking eith ice talking eith yo
+                talking eith you
+              </LastMsg>
+            </ProfileDetail>
+          </User>
+          <User>
+            <ProfileImg src={'/images/pro.jpg'} />
+            <ProfileDetail>
+              <Name>John Doe</Name>
+              <LastMsg>
+                it is nice talking eith ice talking eith ice talking eith yo
+                talking eith you
+              </LastMsg>
+            </ProfileDetail>
+          </User>
+          <User>
+            <ProfileImg src={'/images/pro.jpg'} />
+            <ProfileDetail>
+              <Name>John Doe</Name>
+              <LastMsg>
+                it is nice talking eith ice talking eith ice talking eith yo
+                talking eith you
+              </LastMsg>
+            </ProfileDetail>
+          </User>
         </Left>
-        <Right>Right</Right>
+        <Right>
+          <RightTopbar>
+            <LeftBar>
+              <SmallImg src="/images/pro.jpg" />
+              <SmallName>John Doe</SmallName>
+            </LeftBar>
+            <RightBar>Report</RightBar>
+          </RightTopbar>
+          <ChatArea>
+            <RecievedChat>hello</RecievedChat>
+            <SendChat>hi, how are you doing</SendChat>
+          </ChatArea>
+        </Right>
       </ChatCont>
     </Container>
   );
