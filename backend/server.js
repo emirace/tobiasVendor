cf806ca8664898bf11d9e8e132be8c4304d0c668
emirace <emmanuelakwuba57@gmail.com>
2022-07-07 05:47:07 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:3000", "http://localhost:19006"] }));
+// app.use(cors({ origin: ["http://localhost:3000", "http://localhost:19006"] }));
 
 app.use(
   cookieSession({
