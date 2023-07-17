95bb8b7b0ab13fca7c931a4372d691f5abba39be
emirace <emmanuelakwuba57@gmail.com>
2023-07-17 22:49:58 +0100

    f

--- frontend/src/component/EditorComponent.js
+++ frontend/src/component/EditorComponent.js
@@ -1,6 +1,34 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+import {
+  faImage,
+  faLink,
+  faParagraph,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function EditorComponent() {
+const Button = styled.button`
+  /* Add your button styles here */
+  background-color: #e6e6e6;
+  color: #333;
+  padding: 8px 12px;
+  border: none;
+  border-radius: 4px;
+  margin-right: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    background-color: #ccc;
+  }
+
+  svg {
+    margin-right: 8px;
+  }
+`;
+
+const EditorComponent = () => {
   const [content, setContent] = useState([]);
 
   const handleAddParagraph = () => {
@@ -35,11 +63,17 @@ export default function EditorComponent() {
 
   return (
     <div>
-      <button onClick={handleAddParagraph}>Add Paragraph</button>
-      <button onClick={handleAddLink}>Add Link</button>
-      <button onClick={handleAddImage}>Add Image</button>
+      <Button onClick={handleAddParagraph}>
+        <FontAwesomeIcon icon={faParagraph} /> Add Paragraph
+      </Button>
+      <Button onClick={handleAddLink}>
+        <FontAwesomeIcon icon={faLink} /> Add Link
+      </Button>
+      <Button onClick={handleAddImage}>
+        <FontAwesomeIcon icon={faImage} /> Add Image
+      </Button>
       {/* Render the content state */}
       <pre>{JSON.stringify(content, null, 2)}</pre>
     </div>
   );
-}
+};
